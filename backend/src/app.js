f3aac9ee0920dde911c09e63f0ab50ef31fa7b16
IT24510014 <it24510014@my.sliit.lk>
2026-05-03 17:40:17 +0530

    Comment out products route in app.js
    Commented out the products route in app.js.

--- backend/src/app.js
+++ backend/src/app.js
@@ -14,7 +14,7 @@ app.get('/', (req, res) => {
 });
 
 app.use('/api/auth', require('./routes/authRoutes'));
-app.use('/api/products', require('./routes/productRoutes'));
+//app.use('/api/products', require('./routes/productRoutes'));
 app.use('/api/cart', require('./routes/cartRoutes'));
 app.use('/api/orders', require('./routes/orderRoutes'));
 
