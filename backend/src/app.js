0940b18064797de4d440961588eb9d05297595aa
IT24510014 <it24510014@my.sliit.lk>
2026-05-04 22:55:55 +0530

    Add static file serving and new API routes

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const cors = require('cors');
 const connectDB = require('./config/db');
 
@@ -8,6 +9,7 @@ connectDB();
 
 app.use(cors());
 app.use(express.json());
+app.use('/uploads', express.static(path.join(__dirname, '../uploads')));
 
 app.get('/', (req, res) => {
     res.json({ msg: 'Backend is running' });
@@ -17,5 +19,9 @@ app.use('/api/auth', require('./routes/authRoutes'));
 app.use('/api/products', require('./routes/productRoutes'));
 app.use('/api/cart', require('./routes/cartRoutes'));
 app.use('/api/orders', require('./routes/orderRoutes'));
+app.use('/api/appointments', require('./routes/appointmentRoutes'));
+app.use('/api/pets', require('./routes/petRoutes'));
+app.use('/api/admin', require('./routes/adminRoutes'));
+app.use('/api/images', require('./routes/imageRoutes'));
 
 module.exports = app;
