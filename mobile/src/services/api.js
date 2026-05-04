67ed2458342255f3157cc6b7395f9c2db30626f8
IT24510014 <it24510014@my.sliit.lk>
2026-05-05 02:31:46 +0530

    Refactor API URL setup and export API instance

--- mobile/src/services/api.js
+++ mobile/src/services/api.js
@@ -13,12 +13,7 @@ const getDevApiUrl = () => {
     return host ? `http://${host}:5000/api` : '';
 };
 
-const BASE_URL =
-    (__DEV__ && getDevApiUrl()) ||
-    process.env.EXPO_PUBLIC_API_URL ||
-    'http://127.0.0.1:5000';
-
-export const API_URL = BASE_URL.endsWith('/api') ? BASE_URL : `${BASE_URL}/api`;
+export const API_URL = (__DEV__ && getDevApiUrl()) || process.env.EXPO_PUBLIC_API_URL || 'http://127.0.0.1:5000/api';
 
 console.log('API URL =', API_URL);
 
